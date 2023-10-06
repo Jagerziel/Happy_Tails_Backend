3c1ddd2150cdce72724a9152f4b13de71e6d4b26
anna <anna@rcbd.org>
2023-10-06 11:47:40 -0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const {
+    PORT = 4000,
+    MONGODB_URL,
+} = process.env
