9766f11fa91ebb693a8097baf1009cefbd13df29
anna <anna@rcbd.org>
2023-10-06 12:45:03 -0400

    Added pet  model

---
+++ models/petModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PetSchema = new Schema(
+  {
+    uid: { type: String },
+    pid: { type: String },
+    name: { type: String },
+    dob: { type: String },
+    breed: { type: String },
+    image: { type: String },
+    sex: { type: String },
+    weight: { type: String },
+    allergies: { type: String },
+    medications: { type: String },
+    laboratory: { type: String },
+    microchip: { type: String },
+    visit_history: { type: String },
+    primary_color: { type: String },
+    notes: { type: String },
+    primary_vet: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Pet = mongoose.model('Pet', PetSchema);
+
+module.exports = Pet;
