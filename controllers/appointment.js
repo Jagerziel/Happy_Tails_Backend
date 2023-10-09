5bae9c0f6009383bfb5d3be8f984c5134fb6f4de
Jagerziel <fromnytosg@gmail.com>
2023-10-09 13:24:33 -0400

    updated appointment controller

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -8,9 +8,9 @@ appointment.get('/', isAuthenticated, async (req, res) => {
   try {
     // console.log('hi')
     if (req.appointment) {
-      res.json(await find({ uid: req.appointment.uid }));
+      res.json(await Appointment.find({ uid: req.appointment.uid }));
     } else {
-      res.json(await find({ uid: null }));
+      res.json(await Appointment.find({ uid: null }));
     }
   } catch (error) {
     console.log(error);
@@ -21,7 +21,7 @@ appointment.get('/', isAuthenticated, async (req, res) => {
 appointment.post('/', isAuthenticated, async (req, res) => {
   try {
     req.body.uid = req.appointment.uid;
-    const newAppointment = await create(req.body);
+    const newAppointment = await Appointment.create(req.body);
     res.json(newAppointment);
   } catch (error) {
     console.log(error);
@@ -35,7 +35,7 @@ appointment.put('/', isAuthenticated, async (req, res) => {
     const update = { $set: req.body }; // Update with the entire req.body content
     const options = { new: true };
 
-    const updatedAppointment = await findOneAndUpdate(filter, update, options);
+    const updatedAppointment = await Appointment.findOneAndUpdate(filter, update, options);
     res.json(updatedAppointment);
   } catch (error) {
     res.status(400).json(error);
@@ -44,7 +44,7 @@ appointment.put('/', isAuthenticated, async (req, res) => {
 
 appointment.delete('/:id', isAuthenticated, async (req, res) => {
   try {
-    res.json(await findByIdAndRemove(req.params.id));
+    res.json(await Appointment.findByIdAndRemove(req.params.id));
   } catch (error) {
     res.status(400).json(error);
   }
@@ -53,7 +53,7 @@ appointment.delete('/:id', isAuthenticated, async (req, res) => {
 appointment.get('/:id', isAuthenticated, async (req, res) => {
   try {
     const appointmentUid = req.params.id; // Get the UID from the URL parameter
-    const appointmentAppointment = await findOne({ uid: appointmentUid });
+    const appointmentAppointment = await Appointment.findOne({ uid: appointmentUid });
 
     if (appointmentAppointment) {
       res.json({ exists: true }); // Send true response if appointment data exists
