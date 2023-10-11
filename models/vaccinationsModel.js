319e17e83b7b48e33fe1a78a314acef2d2875bfa
anna <anna@rcbd.org>
2023-10-11 15:49:03 -0400

    Updated models

--- models/vaccinationsModel.js
+++ models/vaccinationsModel.js
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
 
 const VaccinationsSchema = new Schema(
   {
@@ -6,6 +6,10 @@ const VaccinationsSchema = new Schema(
     vaccine: { type: String, require: true },
     last_vaccine_date: { type: String },
     expiration_date: { type: String, require: true },
+    pet_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Pet',
+    }
   },
   {
     timestamps: true,
